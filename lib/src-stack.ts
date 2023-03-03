432ac0a8cef0d3db6d25a7328ff30b75905136c0
Art Wu <awu@archimati.com>
2023-03-03 14:38:31 -0500

    add SSL

--- lib/src-stack.ts
+++ lib/src-stack.ts
@@ -1,8 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -25,22 +27,34 @@ export class SrcStack extends cdk.Stack {
       ]
     });
 
-    const cluster = new ecs.Cluster(this, "MyCluster", {
+    const cluster = new ecs.Cluster(this, "WebServiceCluster", {
       vpc: vpc
     });
 
     const repo = ecr.Repository.fromRepositoryName(this, 'tdd-ecr', 'web-service');
     const tag = '0.0.84';
     const image = ecs.ContainerImage.fromEcrRepository(repo, tag);
+    const certArn = 'arn:aws:acm:us-east-2:962199888341:certificate/d5f3d620-8561-485b-9478-c0bc787b1b43';
+    const certificate = acm.Certificate.fromCertificateArn(this, 'Certificate', certArn);
 
     // Create a load-balanced Fargate service and make it public
-    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+    const loadBalancedFargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
       cluster: cluster, // Required
       cpu: 4096, // Default is 256
       desiredCount: 2, // Default is 1
-      taskImageOptions: { image: image },
+      taskImageOptions: {
+        image: image,
+        containerPort: 443
+      },
       memoryLimitMiB: 16384, // Default is 512
-      publicLoadBalancer: true // Default is true
+      publicLoadBalancer: true, // Default is true
+      certificate: certificate,
+      protocol: elbv2.ApplicationProtocol.HTTPS,
+      targetProtocol: elbv2.ApplicationProtocol.HTTPS
+    });
+
+    loadBalancedFargateService.targetGroup.configureHealthCheck({
+      path: "/app",
     });
 
     // example resource
