c6451c34d73f6c02914fab5c708cd9011fd77027
Art Wu <awu@archimati.com>
2023-03-13 22:48:39 -0400

    set up A record with load balancer target

--- lib/web-service-stack.ts
+++ lib/web-service-stack.ts
@@ -7,8 +7,10 @@ import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as rds from 'aws-cdk-lib/aws-rds';
-import { Construct } from 'constructs';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
 
 // extend the props of the stack by adding the vpc type from the SharedInfraStack
 export interface WebServiceStackProps extends cdk.StackProps {
@@ -23,6 +25,8 @@ export class WebServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: WebServiceStackProps) {
     super(scope, id, props);
 
+    const hostedZoneId = 'Z010453623RVJF2NPOIVD';
+    const domain = 'db.aws-dev.thedinnerdaily.com';
     const cpu = 4096; // Default is 256
     const memoryLimitMiB = 16384;
 
@@ -40,7 +44,6 @@ export class WebServiceStack extends cdk.Stack {
       visibilityTimeout: cdk.Duration.seconds(30)
     });
 
-
     const rdsInstance = new rds.DatabaseInstance(this, 'SymfonyDb', {
       engine: rds.DatabaseInstanceEngine.mysql({
         version: rds.MysqlEngineVersion.VER_5_7_38,
@@ -57,14 +60,24 @@ export class WebServiceStack extends cdk.Stack {
       publiclyAccessible: false,
     });
 
-    // Create task role with access to RDS instance
+    // Create ECS task execution role provisioned for ECS Exec connection
     const taskRole = new iam.Role(this, 'EcsTaskRole', {
       assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
     });
-    taskRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy'));
+    // taskRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy'));
     taskRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonRDSDataFullAccess'));
     taskRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSQSFullAccess'));
     taskRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('SecretsManagerReadWrite'));
+    taskRole.addToPolicy(new iam.PolicyStatement({
+      resources: ['*'],
+      actions: [
+        'ssmmessages:CreateControlChannel',
+        'ssmmessages:CreateDataChannel',
+        'ssmmessages:OpenControlChannel',
+        'ssmmessages:OpenDataChannel'
+      ],
+      effect: iam.Effect.ALLOW
+    }));
     rdsInstance.grantConnect(taskRole);
 
     // The code that defines your stack goes here
@@ -73,7 +86,7 @@ export class WebServiceStack extends cdk.Stack {
     });
 
     const repo = ecr.Repository.fromRepositoryName(this, 'tdd-ecr', 'web-service');
-    const tag = '0.0.84';
+    const tag = '0.0.86';
     const image = ecs.ContainerImage.fromEcrRepository(repo, tag);
     const certArn = 'arn:aws:acm:us-east-2:962199888341:certificate/d5f3d620-8561-485b-9478-c0bc787b1b43';
     const certificate = acm.Certificate.fromCertificateArn(this, 'Certificate', certArn);
@@ -125,6 +138,17 @@ export class WebServiceStack extends cdk.Stack {
     });
     // host.allowSshAccessFrom(ec2.Peer.ipv4('24.147.56.174/32'));
     host.allowSshAccessFrom(ec2.Peer.ipv4('0.0.0.0/0'));
+
+    const hostedZone = route53.PublicHostedZone.fromPublicHostedZoneAttributes(this, 'WebServiceHostedZone', {
+      hostedZoneId,
+      zoneName: domain,
+    });
+    // Define the A record
+       new route53.ARecord(this, 'WebServiceARecord', {
+      zone: hostedZone,
+      recordName: domain,
+      target: route53.RecordTarget.fromAlias(new targets.LoadBalancerTarget(loadBalancedFargateService.loadBalancer)),
+    });
 
     const tcp3306 = ec2.Port.tcpRange(3306, 3306);
     rdsInstance.connections.allowFrom(loadBalancedFargateService.service, tcp3306, 'allow from ecs service');
